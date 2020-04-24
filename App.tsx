07a2690d0d94e4abae69d8b55d87a8b2ec283a84
deaconTown <gnerddavia@gmail.com>
2020-04-24 17:49:28 -0500

    Initial Push for ShoppingList React Native

--- App.tsx
+++ App.tsx
@@ -1,19 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native';
+import Header from './components/Header/Header';
+import { uuid } from 'uuidv4';
+import ListItem from './components/ListItem/ListItem';
+import AddItem from './components/AddItem/AddItem';
 
-export default function App() {
+const App = () => {
+  const [items, setItems] = useState([
+    { id: Math.random(), name: 'Milk', qty: 1 },
+    { id: Math.random(), name: 'Bread', qty: 2 },
+    { id: Math.random(), name: 'Eggs', qty: 12 },
+    { id: Math.random(), name: 'Juice', qty: 3 }
+  ]);
+
+  const deleteItem = (id: any) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const itemExists = (name: string) =>{
+    return items.some(function(item) {
+      return item.name.toUpperCase() === name.toUpperCase();
+    }); 
+  }
+   
+  const addItem = (text: string, qty: number) => {
+  if(itemExists(text)){
+      Alert.alert('Error', 'Item already exist');
+    }
+    else{
+
+      if (!text) {
+        Alert.alert('Error', 'Please enter an item');
+      }
+      else {
+        setItems(prevItems => {
+          return [{ id: Math.random(), name: text, qty }, ...prevItems]
+        });
+  
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList data={items} renderItem={({ item }) => (
+        <ListItem
+          item={item}
+          deleteItem={deleteItem}
+        />
+      )}
+        keyExtractor={item => item.id.toString()} />
     </View>
-  );
-}
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60
   },
 });
+
+export default App;
---
+++ components/AddItem/AddItem.tsx
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const AddItem = (props: any) => {
+    const [itemName, setItemName] = useState('');
+    const [itemQty, setItemQty] = useState(1);
+
+    const onChangeName = (name: any) => setItemName(name);
+    const onChangeQty = (qty: any) => setItemQty(qty);
+
+    return (
+        <View>
+            <TextInput placeholder='Add Item' 
+            style={styles.input} 
+            onChangeText={onChangeName}
+            autoCapitalize = "words"
+            autoCorrect = {true}
+            autoCompleteType = "name"
+            autoFocus= {true}
+            keyboardType = "default"
+            />
+            <TextInput placeholder='Add Item Quantity' 
+            style={styles.input}
+            onChangeText={onChangeQty}
+            keyboardType = "numbers-and-punctuation"
+            />
+            <TouchableOpacity style={styles.btn} onPress={()=> props.addItem(itemName, itemQty)}>
+                <Text style={styles.btnText}>
+                    <Ionicons name='md-add' size={20} />Add Item
+                    </Text>
+            </TouchableOpacity>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    input: {
+        height: 60,
+        padding: 8,
+        fontSize: 16
+    },
+    btn: {
+        backgroundColor: '#c2bad8',
+        padding: 8,
+        margin: 5
+    },
+    btnText: {
+        color: 'darkslateblue',
+        fontSize: 20,
+        textAlign: 'center'
+    }
+});
+
+export default AddItem;
