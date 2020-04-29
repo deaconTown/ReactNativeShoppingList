df3baed45b63cd057ab67b3d7d9e1eeda7527f9c
deaconTown <gnerddavia@gmail.com>
2020-04-29 13:48:44 -0500

    Added a simple navigation
    #6ggwu5[InReview]

---
+++ components/ShoppingList/ShoppingList.tsx
@@ -0,0 +1,103 @@
+import React, { useState,useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Alert, ActivityIndicator } from 'react-native';
+import Header from '../Header/Header';
+import AddItem from '../AddItem/AddItem';
+import ListItem from '../ListItem/ListItem';
+
+
+const ShippingList = (props : any) => {
+  const [items, setItems] = useState([
+    { id: Math.random(), name: 'Milk', qty: 1 },
+    { id: Math.random(), name: 'Bread2', qty: 2 },
+    { id: Math.random(), name: 'Eggs', qty: 12 },
+    { id: Math.random(), name: 'Juice', qty: 3 }
+  ]);
+
+  const [movie, setMovie] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const deleteItem = (id: any) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const itemExists = (name: string) =>{
+    return items.some(function(item) {
+      return item.name.toUpperCase() === name.toUpperCase();
+    }); 
+  }
+   
+  const addItem = (text: string, qty: number) => {
+  if(itemExists(text)){
+      Alert.alert('Error', 'Item already exist');
+    }
+    else{
+
+      if (!text) {
+        Alert.alert('Error', 'Please enter an item');
+      }
+      else {
+        setItems(prevItems => {
+          return [{ id: Math.random(), name: text, qty }, ...prevItems]
+        });
+  
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => setMovie(json.movies))
+      .catch((error) => console.error(error))
+      .finally(() =>  setLoading(false));
+  }, []);
+
+  console.log(movie)
+
+
+  return (
+    <>
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={addItem} />
+
+      <FlatList style={styles.flatList} data={items} renderItem={({ item }) => (
+        <ListItem
+          item={item}
+          deleteItem={deleteItem}
+        />
+      )}
+        keyExtractor={item => item.id.toString()} />
+    </View>
+
+    {/* <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={movie}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )}
+    </View> */}
+    </>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+    minHeight:100,
+    position: 'relative'
+  },
+  flatList:{
+    position: 'relative',
+    display: 'flex'
+  }
+});
+
+export default ShippingList;
---
+++ routes/homeStack.ts
@@ -0,0 +1,32 @@
+import 'react-native-gesture-handler';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import ShoppingList from '../components/ShoppingList/ShoppingList'
+
+const screens = {
+    Home: {
+        screen: ShoppingList
+    },
+    // Meals: {
+    //     screen: () => {}
+    // }
+}
+
+/* FOR RESOLVING NAVIGATION ISSUE 
+https://www.youtube.com/watch?v=cS4PgI3zBzY - WAS USING V4 AND I WAS GETTING ISSUES
+https://reactnavigation.org/docs/getting-started
+yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context (i THINK THIS PART WAS WHAT RESOLVED IT)
+
+FROM THE SITE
+
+[even though this is expo, i believe this was what got it to work as this was the last thing i installed]
+Installing dependencies into a bare React Native project
+yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+Installing dependencies into an Expo managed project: 
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+*/
+
+const HomeStack = createStackNavigator(screens);
+
+export default createAppContainer(HomeStack);
